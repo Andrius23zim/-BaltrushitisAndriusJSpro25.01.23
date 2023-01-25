2b7117e76a30c29249c4315fbe875391275f6f35
Baltrush23 <110633010+Andrius23zim@users.noreply.github.com>
2023-01-25 23:20:37 +0200

    Dz-4changes
    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ console.log(numOrStr);
 
 switch (true) {
     case (numOrStr === null):
-        console.log('ви скасували');
+        console.log('ви скасували'); 
         break;
     case (numOrStr.trim() === ''):
         console.log('Empty String');
